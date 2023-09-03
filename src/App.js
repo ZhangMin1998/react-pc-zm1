dc15dce4f4c6a96b1c208980a6d94ab9ad1db1d5
ZhangMin1998 <1464253423@qq.com>
2023-09-03 14:13:38 +0800

    配置基础路由

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+// 导入路由
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// 导入页面组件
+import Login from './pages/Login'
+import Layout from './pages/Layout'
+
+// 配置路由规则
 function App() {
   return (
-    <div className="App">
-      app
-    </div>
-  );
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Layout/>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
