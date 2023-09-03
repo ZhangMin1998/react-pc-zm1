048b8b5eeb0bbc7251466d238104f7d34ea23078
ZhangMin1998 <1464253423@qq.com>
2023-09-03 16:07:40 +0800

    登录页静态结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,13 @@
+import { Card } from 'antd'
+import logo from '@/assets/image/logo.png'
+import './index.scss'
+
 const Login = () => {
   return (
-    <div>
-      login
+    <div className='login'>
+      <Card className='login_container'>
+        <img className='login_logo' src={logo} alt='logo' />
+      </Card>
     </div>
   )
 }
