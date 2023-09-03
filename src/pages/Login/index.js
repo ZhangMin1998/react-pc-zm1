fd5031139e078c6474bd3a9aa73c3909727ae61e
ZhangMin1998 <1464253423@qq.com>
2023-09-03 16:31:28 +0800

    完成登录表单结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,4 @@
-import { Card } from 'antd'
+import { Card, Form, Input, Button, Checkbox } from 'antd'
 import logo from '@/assets/image/logo.png'
 import './index.scss'
 
@@ -7,6 +7,28 @@ const Login = () => {
     <div className='login'>
       <Card className='login_container'>
         <img className='login_logo' src={logo} alt='logo' />
+        <Form
+          initialValues={{
+            remember: true,
+          }}
+        >
+          <Form.Item>
+            <Input size="large" placeholder='请输入手机号' />
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder='请输入验证码' />
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked">
+            <Checkbox className='login_checkbox_label'>
+              我已阅读并同意「用户协议」和「隐私条款」
+            </Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button type='primary' size='large' htmlType='submit' block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
       </Card>
     </div>
   )
