9b9e3967b9c83cf6ec50e5053ec62488a4800270
ZhangMin1998 <1464253423@qq.com>
2023-09-05 22:30:53 +0800

    layout-echarts基础使用

---
+++ src/components/Bar/index.js
@@ -0,0 +1,30 @@
+// import * as echarts from 'echarts'
+
+// const initChart = () => {
+//   const mychart = echarts.init(document.getElementById('main'))
+
+//   mychart.setOption({
+//     title: {
+//       text: '三大框架满意度'
+//     },
+//     tooltip: {},
+//     xAxis: {
+//       data: ['衬衫', '牛仔裤', '毛衣', '丝袜']
+//     },
+//     yAxis: {},
+//     series: [
+//       {
+//         name: '销量',
+//         type: 'bar',
+//         data: [5, 20, 36, 10]
+//       }
+//     ]
+//   })
+// }
+// function Bar () {
+//   return (
+
+//   )
+// }
+
+// export default Bar
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,36 @@
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
+
 function Home () {
+  const domRef = useRef()
+
+  const initChart = () => {
+    const mychart = echarts.init(domRef.current)
+  
+    mychart.setOption({
+      title: {
+        text: '三大框架满意度'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '牛仔裤', '毛衣', '丝袜']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10]
+        }
+      ]
+    })
+  }
+  useEffect(() => {
+    initChart()
+  }, [])
+
   return (
-    <div>
-      home
-    </div>
+    <div ref={domRef} style={{width: '500px', height: '400px'}}></div>
   )
 }
 
