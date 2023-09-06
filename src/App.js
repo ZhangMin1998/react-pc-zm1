5d6e52f8c61ec49b2a0cdfe31b5eb341522bf970
ZhangMin1998 <1464253423@qq.com>
2023-09-07 00:35:07 +0800

    打包体积分析-路由懒加载

--- src/App.js
+++ src/App.js
@@ -6,36 +6,58 @@ import './App.css'
 import AuthRoute from './components/AuthRoute'
 
 // 导入页面组件
-import Login from './pages/Login'
-import SysLayout from '@/pages/Layout'
-import Home from './pages/Home'
-import Article from './pages/Article'
-import Publish from './pages/Publish'
+// import Login from './pages/Login'
+// import SysLayout from '@/pages/Layout'
+// import Home from './pages/Home'
+// import Article from './pages/Article'
+// import Publish from './pages/Publish'
+
+// 导入必要组件
+import { lazy, Suspense } from 'react'
+// 按需导入路由组件
+const Login = lazy(() => import('./pages/Login'))
+const SysLayout = lazy(() => import('@/pages/Layout'))
+const Home = lazy(() => import('./pages/Home'))
+const Article = lazy(() => import('./pages/Article'))
+const Publish = lazy(() => import('./pages/Publish'))
 
 // 配置路由规则
 function App() {
   return (
     <HistoryRouter history={history}>
-      <div className="App">
-        <Routes>
-          {/* 需要鉴权的路由 */}
-          <Route
-            path="/"
-            element={
-              <AuthRoute>
-                <SysLayout/>
-              </AuthRoute>
-            }
+      <Suspense
+        fallback={
+          <div
+            style={{
+              textAlign: 'center',
+              marginTop: 200
+            }}
           >
-            <Route index element={<Home />}/>
-            <Route path="/article" element={<Article />} />
-            <Route path="/publish" element={<Publish />} />
-          </Route>
+            loading...
+          </div>
+        }
+      >
+        <div className="App">
+          <Routes>
+            {/* 需要鉴权的路由 */}
+            <Route
+              path="/"
+              element={
+                <AuthRoute>
+                  <SysLayout/>
+                </AuthRoute>
+              }
+            >
+              <Route index element={<Home />}/>
+              <Route path="/article" element={<Article />} />
+              <Route path="/publish" element={<Publish />} />
+            </Route>
 
-          {/* 不需要鉴权的路由 */}
-          <Route path="/login" element={<Login/>} />
-        </Routes>
-      </div>
+            {/* 不需要鉴权的路由 */}
+            <Route path="/login" element={<Login/>} />
+          </Routes>
+        </div>
+      </Suspense>
     </HistoryRouter>
   )
 }
